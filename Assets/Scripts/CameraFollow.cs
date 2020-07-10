66a23a8c945f1184dc8403b8a0e54685cba18fdc
Connor William Roy Egan <connorwr.egan@gmail.com>
2020-07-11 07:11:14 +1000

    Camera Follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+[SerializeField]
+Transform target;
+[Space(5)]
+[Header("Boundaries")]
+[SerializeField]
+float minX;
+[SerializeField]
+float maxX;
+[SerializeField]
+public float minY;
+[SerializeField]
+float maxY;
+
+
+
+    void Update()
+    {
+    transform.position = target.position;
+
+
+
+    transform.position = new Vector3(
+    Mathf.Clamp(transform.position.x, minX, maxX),
+    Mathf.Clamp(transform.position.y, minY, maxY),
+    -100
+    );
+    }
+
+
+
+
+
+
+
+
+
+}
