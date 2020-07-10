d08abe6b7ca76b3e7bf93b75c903b3dd6775032e
Connor William Roy Egan <connorwr.egan@gmail.com>
2020-07-11 06:37:29 +1000

    Driving Script

---
+++ Assets/Scripts/Driving.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Driving : MonoBehaviour
+{
+[SerializeField]
+Transform[] waypoints;
+//  SPEED
+[SerializeField]
+float driveSpeed;
+//  This int is the current waypoint's index.
+int currentWayPointIndex = 0;
+
+
+
+    void Update()
+    {
+    if (transform.position == waypoints[currentWayPointIndex].position)
+    ReachNextWaypoint();
+    else
+    transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWayPointIndex].position, driveSpeed * Time.deltaTime);
+    }
+    void ReachNextWaypoint()
+    {
+    Debug.Log("Waypoint " + currentWayPointIndex.ToString() + " reached!");
+    currentWayPointIndex++;
+    }
+
+
+}
