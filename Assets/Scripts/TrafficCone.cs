11a6e3d735135cd7c730205affc7478d08216466
Connor William Roy Egan <connorwr.egan@gmail.com>
2020-07-11 23:32:26 +1000

    Trafic Cone Created

---
+++ Assets/Scripts/TrafficCone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficCone : MonoBehaviour
+{
+Camera cam;
+bool isDragging = false;
+
+    void Start()
+    {
+    cam = Camera.main;
+    }
+    void Update()
+    {
+    Vector3 pos = cam.ScreenToWorldPoint(Input.mousePosition);
+    pos.z = 0f;
+    if (isDragging)
+    transform.position = pos;
+    }
+    void OnMouseDown()
+    {
+    isDragging = true;
+    }
+    void OnMouseUp()
+    {
+    isDragging = false;
+    }
+
+
+
+
+}
