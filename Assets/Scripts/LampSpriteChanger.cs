c600f0ae992e17b59600259e00da7629af08b9bd
Vincent Song <dela.vincent.15@gmail.com>
2020-07-11 15:49:19 -0400

    Lamp Sprite Changer

---
+++ Assets/Scripts/LampSpriteChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampSpriteChanger : MonoBehaviour
+{
+   private Sprite brokenL, brokenR;
+   private BoxCollider2D coll;
+   private SpriteRenderer rend;
+
+   private void Start(){
+   	   rend = GetComponent<SpriteRenderer>();
+       brokenL = Resources.Load<Sprite>("broken_lamp_L");
+       brokenR = Resources.Load<Sprite>("broken_lamp_R");
+       coll = GetComponent<BoxCollider2D>();
+   }
+
+   void OnCollisionEnter2D(Collision2D coll){
+    if(gameObject.tag == "Left"){
+        rend.sprite = brokenL;
+    }
+    if(gameObject.tag == "Right"){   
+        rend.sprite = brokenR;
+	}
+   }
+}
