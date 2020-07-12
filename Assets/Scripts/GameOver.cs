0f707cfaf0bf82b8a01f41c20652350bc3fe3689
Andreas (Fniss) <andreas.poygeist@gmail.com>
2020-07-12 20:43:05 +0200

    sound fix,

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -9,10 +9,12 @@ public TextMeshProUGUI scoreText;
     public void OnEnable()
     {
         scoreText.text = FindObjectOfType<PlayerHealth>().currentPoints.ToString();
+        Time.timeScale = 0;
     }
 
     public void Restart()
     {
+        Time.timeScale = 1;
     SceneManager.LoadScene(0);
     }
     public void Quit()
