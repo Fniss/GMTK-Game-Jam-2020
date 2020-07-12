2d171de9635c85f2a43bdece33faa02ec7359240
Andreas (Fniss) <andreas.poygeist@gmail.com>
2020-07-12 17:46:06 +0200

    startscreen connected, trying to fix cones again

---
+++ Assets/MoveToScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveToScreen : MonoBehaviour
+{
+    public int screenIndex;
+   void Update()
+   {
+       if (Input.anyKeyDown)
+       {
+           OnButtonPress();
+       }
+   }
+   public void OnButtonPress()
+   {
+        SceneManager.LoadScene(screenIndex);
+   }
+}
--- Assets/Scripts/TrafficCone.cs
+++ Assets/Scripts/TrafficCone.cs
@@ -3,38 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TrafficCone : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+public class TrafficCone : MonoBehaviour, IBeginDragHandler,IEndDragHandler, IDragHandler
 {
 public Camera cam;
 bool isDragging = false;
 
     void Awake()
     {
-    cam = FindObjectOfType	<Camera>();
+    // cam = FindObjectOfType	<Camera>();
     }
     void Update()
     {
-    Vector3 pos = cam.ScreenToWorldPoint(Input.mousePosition);
-    pos.z = 0f;
-    if (isDragging)
-    transform.position = pos;
+    // Vector3 pos = cam.ScreenToWorldPoint(Input.mousePosition);
+    // pos.z = 0f;
+    // if (isDragging)
+    // transform.position = pos;
     }
-    void OnMouseDown()
+    // void OnMouseDown()
+    // {
+    // isDragging = true;
+    // }
+    // void OnMouseUp()
+    // {
+    // isDragging = false;
+    // }
+
+    // public void OnPointerDown(PointerEventData eventData)
+    // {
+    // isDragging = true;
+    // }
+
+    // public void OnPointerUp(PointerEventData eventData)
+    // {
+    // isDragging = false;
+    // }
+
+    public void OnDrag(PointerEventData eventData)
     {
-    isDragging = true;
-    }
-    void OnMouseUp()
-    {
-    isDragging = false;
+        transform.position += (Vector3)eventData.delta;
     }
 
-    public void OnPointerDown(PointerEventData eventData)
+    public void OnBeginDrag(PointerEventData eventData)
     {
-    isDragging = true;
     }
 
-    public void OnPointerUp(PointerEventData eventData)
-    {
-    isDragging = false;
+    public void OnEndDrag(PointerEventData eventData){
     }
 }
